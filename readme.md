41914396f11b6d982dc6edd87f0ecff2abcf35e5
Óscar <o.gonzalez@radioecca.org>
2015-11-19 18:15:46 +0000

    First commit

---
+++ readme.md
@@ -0,0 +1,15 @@
+# SQL Logging
+
+A package which allows to log SQL queries in Laravel
+
+## Installation
+
+Begin by installing this package through Composer.
+
+```js
+{
+    "require": {
+		"oscaragcp/sql-logging": "dev-master"
+	}
+}
+```
---
+++ src/SqlLoggingServiceProvider.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace OscarAGCP\SqlLogging;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Event;
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
+
+class SqlLoggingServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '/config/sql-logging.php' => config_path('sql-logging.php')
+        ]);
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        if (config('sql-logging.log', false)) {
+            Event::listen('illuminate.query',
+                function($query, $bindings, $time) {
+                $data = compact('bindings', 'time');
+
+                // Format binding data for sql insertion
+                foreach ($bindings as $i => $binding) {
+                    if ($binding instanceof \DateTime) {
+                        $bindings[$i] = $binding->format('\'Y-m-d H:i:s\'');
+                    } else if (is_string($binding)) {
+                        $bindings[$i] = "'$binding'";
+                    }
+                }
+
+                // Insert bindings into query
+                $query = str_replace(array('%', '?'), array('%%', '%s'), $query);
+                $query = vsprintf($query, $bindings);
+
+                $log = new Logger('sql');
+                $log->pushHandler(new StreamHandler(storage_path().'/logs/sql-'.date('Y-m-d').'.log',
+                    Logger::INFO));
+
+                // add records to the log
+                $log->addInfo($query, $data);
+            });
+        }
+
+    }
+}
